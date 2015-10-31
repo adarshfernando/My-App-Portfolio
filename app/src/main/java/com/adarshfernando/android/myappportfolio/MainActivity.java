dbef38cc9448366963fbec62b1fd9588bbc6b46b
adarshfernando <adarsh.fernando@gmail.com>
2015-10-30 21:43:27 -0700

    Linear Layout with Weighted Views.
    1 Text, 6 Buttons

--- app/src/main/java/com/adarshfernando/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/adarshfernando/android/myappportfolio/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 
     @Override
